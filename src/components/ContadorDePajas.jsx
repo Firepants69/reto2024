fe4813295d43bc020c6c501337c6367282a15dd7
¨Rafael <rafaelmerlinprieto@gmail.com>
2024-04-21 23:46:19 -0600

    cambios en la interfaz

--- src/components/ContadorDePajas.jsx
+++ src/components/ContadorDePajas.jsx
@@ -5,40 +5,42 @@ import { contador } from '../helpers/restaFecha';
 export function ContadorDePajas() {
   const [mensaje, setMensaje] = useState(contador());
   const [backGroundSound, setBackGroundSound] = useState(null);
-  const [clase,setClase] = useState('d-flex flex-row-reverse prueba')
-
+  const [clase, setClase] = useState('d-flex flex-row-reverse prueba')
+  const [isPlay, setIsPlay] = useState(false)
   useEffect(() => {
-    
-    // Crea la instancia de Audio cuando el componente se monte
+
     const audio = new Audio('./assets/cancion.mp3');
     audio.loop = true;
     audio.volume = .5;
     setBackGroundSound(audio);
 
     return () => {
-      // Limpia la instancia de Audio cuando el componente se desmonte
       if (backGroundSound) {
         backGroundSound.pause();
       }
     };
   }, []);
-  
-  const onClickSound =()=>{
-    const miBoton = document.getElementById("botonRepro");
+
+  const onClickSound = () => {
     backGroundSound.loop = true;
     backGroundSound.play()
-    miBoton.disabled = true;
-    }
-  
-    // const onClickCaer=()=>{
-    //   const miBoton = document.getElementById("botonRepro");
-    //   const decepcion = new Audio('./assets/decepcion.mp3')
-    //   backGroundSound.loop = false;
-    //   backGroundSound.pause();
-    //   decepcion.volume = 1;
-    //   miBoton.disabled = false;
-    //   decepcion.play()
-    // }
+    setIsPlay(true)
+  }
+
+  const onPause = () => {
+    backGroundSound.pause()
+    setIsPlay(false)
+  }
+
+  // const onClickCaer=()=>{
+  //   const miBoton = document.getElementById("botonRepro");
+  //   const decepcion = new Audio('./assets/decepcion.mp3')
+  //   backGroundSound.loop = false;
+  //   backGroundSound.pause();
+  //   decepcion.volume = 1;
+  //   miBoton.disabled = false;
+  //   decepcion.play()
+  // }
 
   useEffect(() => {
     const intervalo = setInterval(() => {
@@ -50,23 +52,32 @@ export function ContadorDePajas() {
   return (
     <div className="app-container">
       <div className={clase}>
-      <button className="position-relative btn btn-primary px-2 me-3 mt-3"
-          onClick={()=>setClase('contenedorCambio')}>x</button>
-      <button className="position-relative btn btn-primary p-2 me-3 mt-3">Crear Cuenta</button> 
-      <button className="position-relative btn btn-primary p-2 me-3 mt-3">Iniciar Sesion</button>
-      <small className="text-justify me-3 mt-3 texto">Inicia sesion o crea una cuenta para tu propio contador</small>
+        {clase === 'd-flex flex-row-reverse prueba' ? (
+          <button className="position-relative btn btn-outline-primary px-2 me-3 mt-3"
+            onClick={() => setClase('d-flex flex-row-reverse prueba contenedorCambio')}>ocultar</button>
+        ) : (
+          <button className="position-relative btn btn-outline-danger px-2 me-3 mt-3"
+            onClick={() => setClase('d-flex flex-row-reverse prueba')}>No ocultar</button>
+        )}
+        <button className="position-relative btn btn-primary p-2 me-3 mt-3">Crear Cuenta</button>
+        <button className="position-relative btn btn-primary p-2 me-3 mt-3">Iniciar Sesion</button>
+        <small className="text-justify me-3 mt-3 texto">Inicia sesion o crea una cuenta para tu propio contador</small>
       </div>
-      
+
       <video className="video-background" src='./assets/llamas.mp4' autoPlay loop muted></video>
       <div className="content">
         <h1>Tiempo sin pajearme:</h1>
         <h1>{mensaje}</h1>
-        <button 
-          id = "botonRepro" 
-          className='btn btn-primary mt-2 me-3' 
-          onClick={onClickSound}> Reproducir
-        </button>
-        {clase==='contenedorCambio'?<button>hover</button>:null}
+        {!isPlay ? (
+          <button
+            id="botonRepro"
+            className='btn btn-primary mt-2 me-3'
+            onClick={onClickSound}> Reproducir
+          </button>) :
+          (<button
+            className='btn btn-danger mt-2 me-3 mantener-boton'
+            onClick={onPause}
+          >Pausar</button>)}
         {/*<button 
           id = "botoncaida" 
           className='btn btn-primary mt-2' 
@@ -74,7 +85,9 @@ export function ContadorDePajas() {
          > Caer
         </button>*/}
       </div>
-     
+
     </div>
   );
 }
+
+
